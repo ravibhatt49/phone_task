a31d1331cc65e1827e27dc4d37ed464bf2f74c04
ravibhatt49 <ravibhatt49@gmail.com>
2017-11-12 18:59:03 +0530

    setting changes heroku

--- phone_track/settings.py
+++ phone_track/settings.py
@@ -18,8 +18,8 @@ ROOT_DIR = environ.Path(__file__)
 env = environ.Env()
 env.read_env()
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
