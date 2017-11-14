6e27bbc836753e1e6fb347ed4c1a3f2f0bbc3a9a
ravibhatt49 <rbhatt@codal.com>
2017-11-14 18:38:28 +0530

    heroku changes

--- phone_track/settings.py
+++ phone_track/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 print('===========>',os.path.join(BASE_DIR, 'static'))
 # Extra places for collectstatic to find static files.
