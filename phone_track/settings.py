22dce3eaa462d2195cff4362865993f91ff40b19
ravibhatt49 <rbhatt@codal.com>
2017-11-14 19:56:38 +0530

    heroku changes

--- phone_track/settings.py
+++ phone_track/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 
 
-STATIC_ROOT = os.path.join(root, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
