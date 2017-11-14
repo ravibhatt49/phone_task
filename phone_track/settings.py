345c90bcb18dc102a9485f70758920a33e374d90
ravibhatt49 <rbhatt@codal.com>
2017-11-14 18:50:25 +0530

    heroku changes

--- phone_track/settings.py
+++ phone_track/settings.py
@@ -143,5 +143,5 @@ STATIC_URL = '/static/'
 print('===========>',os.path.dirname(os.path.join(BASE_DIR)))
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.dirname(os.path.join(BASE_DIR, 'static')),
+    os.path.dirname(os.path.join(BASE_DIR)),
 )
