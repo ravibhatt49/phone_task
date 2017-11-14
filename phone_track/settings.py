bf2052286444865c846af91395be21608da68324
ravibhatt49 <rbhatt@codal.com>
2017-11-14 18:09:43 +0530

    heroku changes

--- phone_track/settings.py
+++ phone_track/settings.py
@@ -19,7 +19,7 @@ env = environ.Env(DEBUG=(bool, False),) # set default values and casting
 environ.Env.read_env() # reading .env file
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print('===========',BASE_DIR)
 
 
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'phone_track/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
