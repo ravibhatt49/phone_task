f516bd435f57b20bec2408144bc59238d28ce000
ravibhatt49 <rbhatt@codal.com>
2017-11-14 19:55:04 +0530

    heroku changes

--- phone_track/settings.py
+++ phone_track/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'phone_track.urls'
@@ -138,25 +139,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = str(root('staticfiles'))
+# STATIC_ROOT = str(root('staticfiles'))
 
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = (
+#     str(root.path('static')),
+# )
+
+
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    str(root.path('static')),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
-
-
-
-# STATIC_ROOT = os.path.dirname(os.path.dirname(os.path.join(BASE_DIR, 'static')))
-# print('===========>',os.path.dirname(os.path.join(BASE_DIR)))
-# # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.dirname(os.path.join(BASE_DIR,'static')),
-# )
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
